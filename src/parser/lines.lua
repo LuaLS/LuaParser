6a9bed82e30f385e402c1cdead38e7bd1cb5f638
最萌小汐 <sumneko@hotmail.com>
2018-11-28 16:38:34 +0800

    使用缓存获取行列和位置

--- src/parser/lines.lua
+++ src/parser/lines.lua
@@ -29,13 +29,67 @@ Indent  = m.C(m.S' \t')^0,
 local mt = {}
 mt.__index = mt
 
+function mt:position(row, col)
+    if row < 1 then
+        return 1
+    end
+    if row > #self then
+        return self.len + 1
+    end
+    local line = self[row]
+    local next_line = self[row+1]
+    local start = line[1]
+    local finish
+    if next_line then
+        finish = next_line[1] - 1
+    else
+        finish = self.len + 1
+    end
+    local pos = start + col - 1
+    if pos < start then
+        pos = start
+    elseif pos > finish then
+        pos = finish
+    end
+    return pos
+end
 
+function mt:rowcol(pos)
+    if pos < 1 then
+        return 1, 1
+    end
+    if pos > self.len + 1 then
+        return #self, math.max(1, self.len - self[#self][1] + 2)
+    end
+    local min = 1
+    local max = #self
+    for _ = 1, 100 do
+        local row = (max - min) // 2 + min
+        local start = self[row][1]
+        if pos < start then
+            max = row
+        elseif pos > start then
+            local next_start = self[row + 1][1]
+            if pos < next_start then
+                return row, pos - start + 1
+            elseif pos > next_start then
+                min = row
+            else
+                return row + 1, 1
+            end
+        else
+            return row, 1
+        end
+    end
+    error('rowcol failed!')
+end
 
 return function (self, buf)
     local lines, err = parser:match(buf)
     if not lines then
         return nil, err
     end
+    lines.len = #buf
 
     return setmetatable(lines, mt)
 end
--- test/lines.lua
+++ test/lines.lua
@@ -12,3 +12,39 @@ assert(lines[358][2] == 2)
 assert(lines[358][3] == 0)
 assert(lines[359][2] == 0)
 assert(lines[359][3] == 1)
+
+assert(lines.len     == #buf)
+
+local row, col = lines:rowcol(618)
+assert(row == 22)
+assert(col == 19)
+
+local pos = lines:position(22, 19)
+assert(pos == 618)
+
+local row, col = lines:rowcol(9999999)
+assert(row == 365)
+assert(col == 1)
+
+local row, col = lines:rowcol(-100)
+assert(row == 1)
+assert(col == 1)
+
+local pos = lines:position(8, 999)
+assert(pos == 293)
+
+local pos = lines:position(8, -100)
+assert(pos == 270)
+
+local pos = lines:position(-100, 1)
+assert(pos == 1)
+
+local pos = lines:position(9999, 1)
+assert(pos == 10373)
+
+local pos = lines:position(365, 1)
+assert(pos == 10373)
+
+local row, col = lines:rowcol(10373)
+assert(row == 365)
+assert(col == 1)
