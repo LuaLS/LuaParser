4e1d22604e5dc2b015c593147659ed98d305c97c
最萌小汐 <sumneko@hotmail.com>
2018-12-08 18:08:07 +0800

    打印文件大小

--- test/lines.lua
+++ test/lines.lua
@@ -2,7 +2,7 @@
 local parser = require 'parser'
 local buf = io.load(ROOT / 'src' / 'parser' / 'relabel.lua')
 assert(buf)
-
+print(#buf)
 local lines = parser:lines(buf)
 
 assert(#lines          == 365)
