d4e005552f678215e3b1651282d38d323b3495e9
sumneko <sumneko@hotmail.com>
2019-04-23 20:40:56 +0800

    改革文件名试试

--- test/lines.lua
+++ test/lines.lua
@@ -1,6 +1,6 @@
 
 local parser = require 'parser'
-local buf = io.load(ROOT / 'test' / 'perform' / 'relabel.txt')
+local buf = io.load(ROOT / 'test' / 'perform' / 'lines.txt')
 assert(buf)
 local lines = parser:lines(buf)
 
