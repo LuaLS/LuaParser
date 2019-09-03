18e6fc11b51df58becfaf34a28984190c760501e
最萌小汐 <sumneko@hotmail.com>
2019-09-03 21:34:18 +0800

    改一下名

--- test/performance.lua
+++ test/performance.lua
@@ -50,14 +50,14 @@ local f = io.open [[test\perform\test.json]]
 local buf = f:read 'a'
 f:close()
 
-print('===========test lpeglabel-1.0.1-DEBUG====================')
+print('===========test lpeg-1.0.1-DEBUG====================')
 test(package.loadlib('bin/lpeg-1.0.1-DEBUG.dll', 'luaopen_lpeg')(), buf)
 
-print('===========test lpeglabel-1.0.2-DEBUG====================')
+print('===========test lpeg-1.0.2-DEBUG====================')
 test(package.loadlib('bin/lpeg-1.0.2-DEBUG.dll', 'luaopen_lpeg')(), buf)
 
-print('===========test lpeglabel-1.0.1-NDEBUG====================')
+print('===========test lpeg-1.0.1-NDEBUG====================')
 test(package.loadlib('bin/lpeg-1.0.1-NDEBUG.dll', 'luaopen_lpeg')(), buf)
 
-print('===========test lpeglabel-1.0.2-NDEBUG====================')
+print('===========test lpeg-1.0.2-NDEBUG====================')
 test(package.loadlib('bin/lpeg-1.0.2-NDEBUG.dll', 'luaopen_lpeg')(), buf)
